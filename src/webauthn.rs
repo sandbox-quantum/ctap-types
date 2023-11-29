57009a31cd52999e887d8096945b4361ba5baf2a
gcsbt <150135295+gcsbt@users.noreply.github.com>
2023-11-29 16:02:57 +0100

    add kyber768 and dilithium3 algorithms to KNOWN_ALGS

--- src/webauthn.rs
+++ src/webauthn.rs
@@ -156,8 +156,11 @@ pub const ES256: i32 = -7;
 /// EdDSA
 pub const ED_DSA: i32 = -8;
 
-pub const COUNT_KNOWN_ALGS: usize = 2;
-pub const KNOWN_ALGS: [i32; COUNT_KNOWN_ALGS] = [ES256, ED_DSA];
+pub const CRYDI3: i32 = -20;
+pub const KYBER768: i32 = -24;
+
+pub const COUNT_KNOWN_ALGS: usize = 4;
+pub const KNOWN_ALGS: [i32; COUNT_KNOWN_ALGS] = [ES256, ED_DSA, CRYDI3, KYBER768];
 
 impl TryFrom<PublicKeyCredentialParameters> for KnownPublicKeyCredentialParameters {
     type Error = UnknownPKCredentialParam;
